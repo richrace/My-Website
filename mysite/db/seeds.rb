5aadd2181d0d2f99bb3f2090f183ea78e7dd242f
Richard Race <race.richard@gmail.com>
2013-01-21 20:26:25 +0000

    Can't remember

--- mysite/db/seeds.rb
+++ mysite/db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -7,9 +9,9 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
 projects = Project.create({
-	:title => "XBMC Remote App",
-	:description => "An iPhone and Android XBMC Remote App built using Ruby.",
-	:content => "Bigger description here",
-	:github => "https://github.com/richrace/RemoteApp"
+	:title => "XBMC Raspberry Pi",
+	:description => "What I'm going to do with my Raspberry Pi",
+	:content => "So my plan is to use OpenElec and install it onto my Raspberry Pi. However, as I'm currently poor, I don't have a spare HDMI cable (I know, only £2) or a Micro USB charger. I will update this once I get started with it!"
 	})
+
 
